afcc4dfad1563770f780bccfd0bb89a9cabef613
flanjersi <grosj83670@gmail.com>
2017-11-17 15:22:30 +0100

    fuck

--- launch.sh
+++ launch.sh
@@ -1,24 +1,24 @@
 cd VM1
-echo -e "Démarrage VM1"
+echo "Démarrage VM1\n"
 vagrant up
 
 cd ../VM2
-echo -e "Démarrage VM2"
+echo "Démarrage VM2\n"
 vagrant up
 
 cd ../VM3
-echo -e "Démarrage VM3"
+echo "Démarrage VM3\n"
 vagrant up
 
 cd ../VM1-6
-echo -e "Démarrage VM1-6"
+echo "Démarrage VM1-6\n"
 vagrant up
 
 
 cd ../VM2-6
-echo -e "Démarrage VM2-6"
+echo "Démarrage VM2-6\n"
 vagrant up
 
 cd ../VM3-6
-echo -e "Démarrage VM3-6"
+echo "Démarrage VM3-6\n"
 vagrant up
